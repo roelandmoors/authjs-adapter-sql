5c6f73d8e4d12903bb09d419827a3dfd496cc5f4
roeland <roeland@lan.art>
2023-05-01 20:17:26 +0200

    accounts repo

--- src/mysql2.ts
+++ src/mysql2.ts
@@ -1,21 +1,21 @@
 import type { Connection, ResultSetHeader, RowDataPacket } from "mysql2/promise";
-import { ExecuteResult, SqlHelpers } from "./db";
+import { ExecuteResult, Primitive, SqlHelpers } from "./db";
 
 export default function buildMysql2Helpers(getConnection: () => Promise<Connection>): SqlHelpers {
-  const execute = async (sql: string, values: any[]): Promise<ExecuteResult> => {
+  const execute = async (sql: TemplateStringsArray, ...values: Primitive[]): Promise<ExecuteResult> => {
     const conn = await getConnection();
     try {
-      const result = (await conn.execute(sql, values)) as ResultSetHeader[];
+      const result = (await conn.execute(sql.join("?"), values)) as ResultSetHeader[];
       return { insertId: Number(result[0].insertId) };
     } finally {
       await conn.end();
     }
   };
 
-  const query = async <T>(sql: string, values: any[]): Promise<T[]> => {
+  const query = async <T>(sql: TemplateStringsArray, ...values: Primitive[]): Promise<T[]> => {
     const conn = await getConnection();
     try {
-      const [rows] = await conn.query<T[] & RowDataPacket[][]>(sql, values);
+      const [rows] = await conn.query<T[] & RowDataPacket[][]>(sql.join("?"), values);
       return rows;
     } finally {
       await conn.end();
