cd598c9c109397af2aa70cffa2d8edc1c45dc9a7
roeland <roeland@lan.art>
2023-05-14 19:56:44 +0200

    tests are working

--- src/drivers/kysely.ts
+++ src/drivers/kysely.ts
@@ -1,32 +1,19 @@
-import { Kysely, RawBuilder, RawNode, sql } from "kysely";
+import { Kysely, sql } from "kysely";
 import type { ExecuteResult, Primitive, Sql, SqlHelpers } from "../types";
-import { createQueryId } from "kysely/dist/cjs/util/query-id";
-import { parseValueExpression } from "kysely/dist/cjs/parser/value-parser";
 
 const dialect = "mysql";
 
 export default function buildKyselyHelpers(getConnection: () => Kysely<unknown>): SqlHelpers {
   const execute = async (sqlStmt: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
     const db = await getConnection();
-    //const builder = createRawBuilder(sql, ...values);
-    //const result = await builder.execute(db);
     const result = await sql(sqlStmt as TemplateStringsArray, ...values).execute(db);
     return { insertId: Number(result.insertId) };
   };
 
   const query = async <T>(sqlStmt: Sql, ...values: Primitive[]): Promise<T[]> => {
     const db = await getConnection();
-    //const builder = createRawBuilder<T>(sql, ...values);
-    //const result = await builder.execute(db);
     const result = await sql<T>(sqlStmt as TemplateStringsArray, ...values).execute(db);
     return result.rows;
   };
   return { execute, query, dialect };
 }
-
-// function createRawBuilder<T>(sql: Sql, ...values: Primitive[]): RawBuilder<T> {
-//   return new RawBuilder({
-//     queryId: createQueryId(),
-//     rawNode: RawNode.create(sql, values?.map(parseValueExpression) ?? []),
-//   });
-// }
