c6020e7407c7310def13d08247067f542eae482d
roeland <roeland@lan.art>
2023-05-14 19:36:57 +0200

    most tests work

--- src/drivers/kysely.ts
+++ src/drivers/kysely.ts
@@ -1,21 +1,32 @@
-import { Kysely, sql as ksql } from "kysely";
+import { Kysely, RawBuilder, RawNode, sql } from "kysely";
 import type { ExecuteResult, Primitive, Sql, SqlHelpers } from "../types";
+import { createQueryId } from "kysely/dist/cjs/util/query-id";
+import { parseValueExpression } from "kysely/dist/cjs/parser/value-parser";
 
 const dialect = "mysql";
 
 export default function buildKyselyHelpers(getConnection: () => Kysely<unknown>): SqlHelpers {
-  const execute = async (sql: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
+  const execute = async (sqlStmt: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
     const db = await getConnection();
-    const tsql = sql as TemplateStringsArray;
-    const result = await ksql(tsql, ...values).execute(db);
+    //const builder = createRawBuilder(sql, ...values);
+    //const result = await builder.execute(db);
+    const result = await sql(sqlStmt as TemplateStringsArray, ...values).execute(db);
     return { insertId: Number(result.insertId) };
   };
 
-  const query = async <T>(sql: Sql, ...values: Primitive[]): Promise<T[]> => {
+  const query = async <T>(sqlStmt: Sql, ...values: Primitive[]): Promise<T[]> => {
     const db = await getConnection();
-    const tsql = sql as TemplateStringsArray;
-    const result = await ksql<T>(tsql, ...values).execute(db);
+    //const builder = createRawBuilder<T>(sql, ...values);
+    //const result = await builder.execute(db);
+    const result = await sql<T>(sqlStmt as TemplateStringsArray, ...values).execute(db);
     return result.rows;
   };
   return { execute, query, dialect };
 }
+
+// function createRawBuilder<T>(sql: Sql, ...values: Primitive[]): RawBuilder<T> {
+//   return new RawBuilder({
+//     queryId: createQueryId(),
+//     rawNode: RawNode.create(sql, values?.map(parseValueExpression) ?? []),
+//   });
+// }
--- tests/kysely.test.ts
+++ tests/kysely.test.ts
@@ -6,24 +6,34 @@ import dbTests from "./shared";
 import { Kysely, MysqlDialect } from "kysely";
 import * as mysql from "mysql2";
 
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE || "authjs_test",
+});
+
 function getConnection() {
   const db = new Kysely({
     dialect: new MysqlDialect({
-      pool: mysql.createPool({
-        host: "127.0.0.1",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE || "authjs_test",
-      }),
+      pool,
     }),
   });
   return db;
 }
 
 const helpers = builKyselyHelpers(getConnection);
-const db = buildUnitOfWork(helpers);
+const uow = buildUnitOfWork(helpers);
+
+// Close pool after tests
+let dbTestsWithDisconnect = {
+  ...dbTests(uow),
+  disconnect: async () => {
+    await pool.end();
+  },
+};
 
 runBasicTests({
   adapter: SqlAdapter(helpers),
-  db: dbTests(db),
+  db: dbTestsWithDisconnect,
 });
