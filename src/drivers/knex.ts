b4c48d9040465cffe57ab229f3f5ce352991725f
roeland <roeland@lan.art>
2023-06-23 19:45:54 +0200

    knex

--- src/drivers/knex.ts
+++ src/drivers/knex.ts
@@ -1,24 +1,30 @@
 import type { Knex } from "knex";
-import { Dialect, ExecuteResult, Primitive, Sql, SqlHelpers } from "../types";
-import { buildParameterizedSql } from "../utils";
+import { Dialect } from "../types";
 
-export function buildKnexHelpers(knex: Knex, dialect: Dialect): SqlHelpers {
-  const execute = async (sql: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
-    const paramSql = buildParameterizedSql(sql, "mysql");
-    const result = await knex.raw(paramSql, values);
-    if (dialect == "mysql") return { insertId: Number(result[0].insertId) };
-    let insertId = 0;
-    if (result.rows && result.rows[0]) insertId = (result.rows[0] as any)["id"];
-    return { insertId };
-  };
+import { SqlTag, createSqlTag, Mysql2Driver } from "sql-tagged-template";
 
-  const query = async <T>(sql: Sql, ...values: Primitive[]): Promise<T[]> => {
-    const paramSql = buildParameterizedSql(sql, "mysql");
-    const result = await knex.raw(paramSql, values);
-    if (dialect == "mysql") return result[0];
-    return result.rows;
-  };
-  return { execute, query, dialect };
+export function buildKnexHelpers(knex: Knex, dialect: Dialect): SqlTag {
+  const driver = new KnexDriver(knex);
+  return createSqlTag(driver);
 }
+
+// export function buildKnexHelpers(knex: Knex, dialect: Dialect): SqlHelpers {
+//   const execute = async (sql: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
+//     const paramSql = buildParameterizedSql(sql, "mysql");
+//     const result = await knex.raw(paramSql, values);
+//     if (dialect == "mysql") return { insertId: Number(result[0].insertId) };
+//     let insertId = 0;
+//     if (result.rows && result.rows[0]) insertId = (result.rows[0] as any)["id"];
+//     return { insertId };
+//   };
+
+//   const query = async <T>(sql: Sql, ...values: Primitive[]): Promise<T[]> => {
+//     const paramSql = buildParameterizedSql(sql, "mysql");
+//     const result = await knex.raw(paramSql, values);
+//     if (dialect == "mysql") return result[0];
+//     return result.rows;
+//   };
+//   return { execute, query, dialect };
+// }
 
 export default buildKnexHelpers;
