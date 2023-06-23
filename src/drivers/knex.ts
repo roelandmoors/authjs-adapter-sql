850ceac2e9afed7ee3312e59f5e1850976cb4f9e
roeland <roeland@lan.art>
2023-06-23 19:46:10 +0200

    knex

--- src/drivers/knex.ts
+++ src/drivers/knex.ts
@@ -1,30 +1,11 @@
 import type { Knex } from "knex";
 import { Dialect } from "../types";
 
-import { SqlTag, createSqlTag, Mysql2Driver } from "sql-tagged-template";
+import { SqlTag, createSqlTag, KnexDriver } from "sql-tagged-template";
 
 export function buildKnexHelpers(knex: Knex, dialect: Dialect): SqlTag {
-  const driver = new KnexDriver(knex);
-  return createSqlTag(driver);
+  const driver = new KnexDriver(knex, dialect);
+  return createSqlTag(driver, { addReturningId: dialect === "postgres" });
 }
-
-// export function buildKnexHelpers(knex: Knex, dialect: Dialect): SqlHelpers {
-//   const execute = async (sql: Sql, ...values: Primitive[]): Promise<ExecuteResult> => {
-//     const paramSql = buildParameterizedSql(sql, "mysql");
-//     const result = await knex.raw(paramSql, values);
-//     if (dialect == "mysql") return { insertId: Number(result[0].insertId) };
-//     let insertId = 0;
-//     if (result.rows && result.rows[0]) insertId = (result.rows[0] as any)["id"];
-//     return { insertId };
-//   };
-
-//   const query = async <T>(sql: Sql, ...values: Primitive[]): Promise<T[]> => {
-//     const paramSql = buildParameterizedSql(sql, "mysql");
-//     const result = await knex.raw(paramSql, values);
-//     if (dialect == "mysql") return result[0];
-//     return result.rows;
-//   };
-//   return { execute, query, dialect };
-// }
 
 export default buildKnexHelpers;
